620a0d18233b2ff8fa0b024c0e8d7b401f3c5ec9
程连杰 <chenglianjie@fanxiapp.com>
2021-09-27 18:14:26 +0800

    文件名加hash

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: __dirname + "/dist",
-    filename: "index_bundle.js",
+    filename: "'[name]-[hash].js",
   },
   module: {
     rules: [
