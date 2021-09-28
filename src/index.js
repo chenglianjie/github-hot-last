0ce636d44e2d428b7d5928cab186167dcb53d209
程连杰 <chenglianjie@fanxiapp.com>
2021-09-28 14:34:08 +0800

    代码提交

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Login from "@/login";
-import "./style/index.scss";
-import "./style/index.css";
-import "./style/index.less";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Login from '@/login'
+import './style/index.scss'
+import './style/index.css'
+import './style/index.less'
 function App() {
+  let a = 5
   return (
-    <div className="box">
-      <Login /> hello webpack5!
+    <div className='box'>
+      <Login /> hello webpack5! hello webpack5! hello webpack5! useDebugValue(hello webpack5!) hello webpack5!
     </div>
-  );
+  )
 }
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
--- src/login.js
+++ src/login.js
@@ -1,10 +1,11 @@
-import React from "react";
-import Logo from "./img/article_1_360p.jpg";
+import React from 'react'
+import Logo from './img/article_1_360p.jpg'
 const Login = () => {
+  let a = 5
   return (
     <div>
       登录模块 <img src={Logo} />
     </div>
-  );
-};
-export default Login;
+  )
+}
+export default Login
