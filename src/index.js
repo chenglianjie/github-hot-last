2d38f22c1d4adcd54f45f40ef0717b40dfd404c8
程连杰 <chenglianjie@fanxiapp.com>
2021-09-28 14:47:11 +0800

    测试

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './style/index.css'
 import './style/index.less'
 function App() {
   let a = 5
+  let b = 5
+  let c = 5
   return (
     <div className='box'>
       <Login /> hello webpack5! hello webpack5! hello webpack5! useDebugValue(hello webpack5!) hello webpack5!
